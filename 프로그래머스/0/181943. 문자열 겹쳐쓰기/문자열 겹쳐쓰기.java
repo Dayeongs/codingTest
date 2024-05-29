853d31cf7992f116d8074962dca5d1fd7e5cfe17
Dayeongs <149662678+Dayeongs@users.noreply.github.com>
2024-05-29 16:59:56 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.12 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -2,8 +2,7 @@ class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         String answer = "";
         
-        StringBuffer sb = new StringBuffer();
-        sb.append(my_string);
+        StringBuffer sb = new StringBuffer(my_string);
         sb.replace(s, s + overwrite_string.length(), overwrite_string);
         answer = sb.toString();
             
