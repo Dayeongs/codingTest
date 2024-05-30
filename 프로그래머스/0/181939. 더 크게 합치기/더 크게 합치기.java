10849609323ad7904cf61632b603022048165bc6
Dayeongs <149662678+Dayeongs@users.noreply.github.com>
2024-05-30 15:09:24 +0900

    [level 0] Title: 더 크게 합치기, Time: 9.03 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,19 +1,7 @@
 class Solution {
     public int solution(int a, int b) {
-        int answer = 0;
-        StringBuilder sb1 = new StringBuilder();
-        sb1.append(a).append(b);
-        int num1 = Integer.parseInt(sb1.toString());
-        
-        StringBuilder sb2 = new StringBuilder();
-        sb2.append(b).append(a);
-        int num2 = Integer.parseInt(sb2.toString());
-        
-        if (num1 >= num2) {
-            answer = num1;
-        } else {
-            answer = num2;
-        }
-        return answer;
+        int ab = Integer.parseInt(a + "" + b);
+        int ba = Integer.parseInt(b + "" + a);
+        return ab >= ba ? ab : ba;
     }
 }
