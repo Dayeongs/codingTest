a07cdcfbc9a427c528c2cf97d635b08289878bfa
Dayeongs <149662678+Dayeongs@users.noreply.github.com>
2024-06-03 16:39:06 +0900

    [level 0] Title: 코드 처리하기, Time: 0.05 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,21 +1,17 @@
 class Solution {
     public String solution(String code) {
-        char[] codeList = code.toCharArray();
-        String ret = "";
+        StringBuilder answer = new StringBuilder();
         int mode = 0;
-        for(int idx = 0;idx <= codeList.length - 1; idx++) {
-            if (codeList[idx] == '1') {
-                mode = 1 - mode;
-            } 
-            if (codeList[idx] != '1') {
-                if (mode == 0 && idx % 2 == 0) {
-                    ret += String.valueOf(codeList[idx]);
-                } 
-                if (mode == 1 && idx % 2 != 0) {
-                    ret += String.valueOf(codeList[idx]);
-                }
+        for (int i = 0; i < code.length(); i++) {
+            char current = code.charAt(i);
+            if (current == '1') {
+                mode = mode == 0 ? 1 : 0;
+                continue;
+            }
+            if (i % 2 == mode) {
+                answer.append(current);
             }
         }
-        return ret.isEmpty() ? "EMPTY" : ret;
+        return answer.length() == 0 ? "EMPTY" : answer.toString();
     }
 }
