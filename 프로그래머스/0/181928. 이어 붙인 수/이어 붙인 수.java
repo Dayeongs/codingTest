2b77b944e4163e7eaf93a252cc6aa7ef446f9288
Dayeongs <149662678+Dayeongs@users.noreply.github.com>
2024-06-03 23:59:07 +0900

    [level 0] Title: 이어 붙인 수, Time: 1.27 ms, Memory: 66.5 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,15 +1,15 @@
 class Solution {
     public int solution(int[] num_list) {
-        String str1 = "";
-        String str2 = "";
+        String even = "";
+        String odd = "";
         for (int i = 0; i < num_list.length; i++) {
             int num = num_list[i];
             if (num%2 == 0) {
-                str1 += Integer.toString(num);
+                even += Integer.toString(num);
             } else {
-                str2 += Integer.toString(num);
+                odd += Integer.toString(num);
             }
         }
-        return Integer.parseInt(str1) + Integer.parseInt(str2);
+        return Integer.parseInt(even) + Integer.parseInt(odd);
     }
 }
