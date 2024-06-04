ecbf0d7c7f984b8291753207de17e6f0e61f4c16
Dayeongs <149662678+Dayeongs@users.noreply.github.com>
2024-06-04 22:55:40 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.05 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -1,11 +1,9 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-
-        int n = 0;
         for(int i = 0; i < queries.length; i++){
-            n = arr[queries[i][0]];
+            int temp = arr[queries[i][0]];
             arr[queries[i][0]] = arr[queries[i][1]];
-            arr[queries[i][1]] = n;
+            arr[queries[i][1]] = temp;
         }
         return arr;
     }
