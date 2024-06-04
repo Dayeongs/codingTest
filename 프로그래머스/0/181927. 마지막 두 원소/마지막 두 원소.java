88db08fb6cb7e2b7900f29ecb406081f56c3389e
Dayeongs <149662678+Dayeongs@users.noreply.github.com>
2024-06-04 20:25:19 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int numSize = num_list.length;
-        int[] answer = new int[numSize+1];
-        for(int i = 0; i < numSize; i++) {
+        int[] answer = new int[num_list.length + 1];
+
+        for(int i = 0; i < num_list.length; i++){
             answer[i] = num_list[i];
         }
-        if (num_list[numSize-2] < num_list[numSize-1]) {
-            answer[numSize] = num_list[numSize-1] - num_list[numSize-2];
-        } else {
-            answer[numSize] = num_list[numSize-1] * 2;
-        }
+
+        answer[num_list.length] = 
+            num_list[num_list.length-1] > num_list[num_list.length-2] ? 
+            num_list[num_list.length-1]-num_list[num_list.length-2] : 
+            num_list[num_list.length-1] * 2;
         return answer;
     }
 }
